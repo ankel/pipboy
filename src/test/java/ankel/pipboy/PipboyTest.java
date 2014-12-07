01c0ba3ef7aedeb79045e75511bd7d1681d674c1
Binh Tran <btran@jive.com>
2014-12-07 11:17:23 -0700

    Hello world

---
+++ src/test/java/ankel/pipboy/PipboyTest.java
@@ -0,0 +1,17 @@
+package ankel.pipboy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by btran on 12/7/14.
+ */
+public class PipboyTest
+{
+  @Test
+  public void test()
+  {
+    assertTrue(true);
+  }
+}
